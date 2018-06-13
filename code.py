95c2c1e34271497b2d004c8ab2dfade7dc56459f
Yanfei Chen <yanfei.chen@qeexo.com>
2018-06-13 10:24:59 -0400

    Added the code to perform the operations between two numbers

---
+++ code.py
@@ -0,0 +1,15 @@
+def add(x, y):
+	return x + y
+
+def minus(x, y):
+	return x - y
+
+def multiple(x, y):
+	return x * y
+
+def divide(x, y):
+	if y == 0:
+		print("error!")
+		return
+	else:
+		return x / y
